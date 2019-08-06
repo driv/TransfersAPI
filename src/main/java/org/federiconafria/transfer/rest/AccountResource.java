159c6f88e3e25966154280685889b19de56d2006
Federico Nafria <federiconafria@gmail.com>
2019-08-06 18:34:18 +0200

    Add Account rest resource
    Add endpoint for new account creation
    Add endpoint for querying specific account by ID
    
    Add API integration tests
    Add libraries for integration tests

---
+++ src/main/java/org/federiconafria/transfer/rest/AccountResource.java
@@ -0,0 +1,62 @@
+package org.federiconafria.transfer.rest;
+
+import org.federiconafria.transfer.entities.Account;
+import org.federiconafria.transfer.entities.AccountBuilder;
+import org.federiconafria.transfer.services.AccountService;
+import org.federiconafria.transfer.services.exceptions.AccountNotFoundException;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/accounts")
+@Produces(MediaType.APPLICATION_JSON)
+public class AccountResource {
+    @Inject
+    private AccountService service;
+
+    @GET
+    @Path("/{id}")
+    public Response getAccount(@PathParam("id") long id) {
+        try {
+            Account account = service.getAccount(id);
+            AccountDTO outputData = new AccountDTO();
+            outputData.id = account.getId();
+            outputData.amount = account.getAmount().getAmount().toString();
+            outputData.user = account.getUser();
+            return Response.ok(outputData).build();
+        } catch (AccountNotFoundException e) {
+            return Response
+                    .status(Response.Status.NOT_FOUND)
+                    .build();
+        }
+    }
+
+    @PUT
+    @Path("/")
+    public Response createAccount(NewAccountDTO inputData) {
+        Account newAccount = new AccountBuilder()
+                .setAmount(inputData.amount)
+                .setUser(inputData.user)
+                .build();
+        Account account = service.createAccount(newAccount);
+        AccountDTO outputData = new AccountDTO();
+        outputData.id = account.getId();
+        outputData.user = account.getUser();
+        outputData.amount = account.getAmount().getAmount().toString();
+        return Response.ok(outputData).build();
+    }
+
+    public static class NewAccountDTO {
+        public String user;
+        public String amount;
+    }
+
+    public static class AccountDTO {
+        public long id;
+        public String user;
+        public String amount;
+    }
+
+}
---
+++ src/test/java/org/federiconafria/transfer/rest/AccountResourceIntegrationTest.java
@@ -0,0 +1,85 @@
+package org.federiconafria.transfer.rest;
+
+import org.federiconafria.transfer.entities.AccountBuilder;
+import org.federiconafria.transfer.services.AccountService;
+import org.federiconafria.transfer.storage.AccountMemoryStorage;
+import org.federiconafria.transfer.storage.MyAccountIdProvider;
+import org.glassfish.hk2.api.Factory;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.assertEquals;
+
+public class AccountResourceIntegrationTest extends JerseyTest {
+
+    private AccountService accountService;
+
+    @Override
+    protected Application configure() {
+        accountService = new AccountService(new AccountMemoryStorage(), new MyAccountIdProvider());
+        accountService.createAccount(new AccountBuilder()
+                .setUser("testUser")
+                .setAmount("12.50")
+                .build());
+        return new ResourceConfig(AccountResource.class)
+                .register(new Binder());
+    }
+
+    @Test
+    public void getAccount_NonExistentId_NOT_FOUNDResponse() {
+        Response response = target("/accounts/9").request().get();
+
+        assertEquals(Response.Status.NOT_FOUND.getStatusCode(), response.getStatus());
+    }
+
+    @Test
+    public void getAccount_correctRequest_OKResponse() {
+        Response response = target("/accounts/1").request().get();
+
+        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+        assertEquals(MediaType.APPLICATION_JSON_TYPE, response.getMediaType());
+        AccountResource.AccountDTO output = response.readEntity(AccountResource.AccountDTO.class);
+        assertEquals("testUser", output.user);
+        assertEquals("12.50", output.amount);
+        assertEquals(1, output.id);
+    }
+
+    @Test
+    public void createAccount_correctRequest_OKResponse() {
+        AccountResource.NewAccountDTO input = new AccountResource.NewAccountDTO();
+        input.amount = "12.55";
+        input.user = "testUser";
+        Response response = target("/accounts/").request().put(Entity.entity(input, MediaType.APPLICATION_JSON_TYPE));
+
+
+        assertEquals("Http response should be 200: ", Response.Status.OK.getStatusCode(), response.getStatus());
+        assertEquals("It should be JSON", MediaType.APPLICATION_JSON_TYPE, response.getMediaType());
+        AccountResource.AccountDTO output = response.readEntity(AccountResource.AccountDTO.class);
+        assertEquals(2, output.id);
+    }
+
+    class Binder extends AbstractBinder {
+        @Override
+        protected void configure() {
+            bindFactory(new Factory<AccountService>() {
+                @Override
+                public AccountService provide() {
+                    return accountService;
+                }
+
+                @Override
+                public void dispose(AccountService instance) {
+
+                }
+            }).to(AccountService.class);
+        }
+    }
+
+}
